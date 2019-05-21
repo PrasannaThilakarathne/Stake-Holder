26701f5ce91cfe7840d606b006222290d76fb210
amathathsarani <amathathsarani.398@gmail.com>
2019-05-21 13:03:57 +0530

    After Merge

--- stake-holder/src/main/java/com/virtusa/inventory/repository/CustomerDetailRepository.java
+++
@@ -1,9 +0,0 @@
-package com.virtusa.inventory.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.virtusa.inventory.modal.CustomerDetail;
-
-public interface CustomerDetailRepository extends JpaRepository<CustomerDetail,Integer> {
-
-}
