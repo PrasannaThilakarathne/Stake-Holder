fe80cc6cc8a76b576b629cf136905794cbde80df
amathathsarani <amathathsarani.398@gmail.com>
2019-05-22 19:29:33 +0530

    Validation fixed

--- stake-holder/src/main/java/com/virtusa/inventory/modal/Category.java
+++ stake-holder/src/main/java/com/virtusa/inventory/modal/Category.java
@@ -8,6 +8,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Category {
@@ -16,7 +18,11 @@ public class Category {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 
 	Integer id;
+
+	@NotNull
 	String type;
+
+	@NotNull
 	Integer pointRange;
 
 	@OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
--- stake-holder/src/main/java/com/virtusa/inventory/modal/LoyaltyCard.java
+++ stake-holder/src/main/java/com/virtusa/inventory/modal/LoyaltyCard.java
@@ -9,6 +9,10 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -18,12 +22,25 @@ public class LoyaltyCard {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 
-	Integer id;
-	String name;
-	String number;
-	Double pointBalance;
-	Date issuedDate;
-	Date expiryDate;
+	private Integer id;
+
+	@NotNull(message = "Name can not be empty")
+	@Size(min = 3)
+	private String name;
+
+	@NotNull(message = "Number can not be empty")
+	@Size(min = 8, max = 8)
+	private String number;
+
+	private Double pointBalance;
+
+	@NotNull
+	@DateTimeFormat(pattern = "dd/MM/yyyy")
+	private Date issuedDate;
+
+	@NotNull
+	@DateTimeFormat(pattern = "dd/MM/yyyy")
+	private Date expiryDate;
 
 	@ManyToOne(cascade = CascadeType.ALL)
 	@JoinColumn
