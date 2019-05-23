cd52153abe3dc7b464d9da95b35c957f1775fa8d
amathathsarani <amathathsarani.398@gmail.com>
2019-05-23 15:13:49 +0530

    Test class

---
+++ stake-holder/src/test/java/com/virtusa/inventory/controller/LoyaltycardControllerTest.java
@@ -0,0 +1,19 @@
+package com.virtusa.inventory.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class LoyaltycardControllerTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testFetchOne() {
+		fail("Not yet implemented");
+	}
+
+}
