0023aacffe9c4f70d8ff63a6a4de1b613bf7cb36
PasinduSandakelum <tmspthilakarathna2017@gmail.com>
2019-05-24 08:51:55 +0530

    modified LoyaltyCardControllerTest

--- stake-holder/src/test/java/com/virtusa/inventory/controller/LoyaltyCardControllerTest.java
+++ stake-holder/src/test/java/com/virtusa/inventory/controller/LoyaltyCardControllerTest.java
@@ -21,51 +21,75 @@ import com.virtusa.inventory.modal.LoyaltyCard;
 public class LoyaltyCardControllerTest {
 
 	@Autowired
-    private MockMvc mockMvc;
-	
+	private MockMvc mockMvc;
+
 	@MockBean
 	private LoyaltyCardCotroller loyaltyCardCotroller;
-	
+
 	@Test
-    public void testLoyaltyCardGetStatusIsOk() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/loyalty/card").contentType(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isOk());
+	public void testLoyaltyCardGetStatusIsOk() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/loyalty/card").contentType(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.status().isOk());
 //                .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON_VALUE))
 //                .andReturn();
 //                .andExpect(MockMvcResultMatchers.content().json());
 
-    }
-	
+	}
+
 	@Test
-    public void testLoyaltyCardGetStatusIsNotFound() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/loyalty/cards").contentType(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().isNotFound());
+	public void testLoyaltyCardGetStatusIsNotFound() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/loyalty/cards").contentType(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.status().isNotFound());
 //                .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON_VALUE))
 //                .andReturn();
 //                .andExpect(MockMvcResultMatchers.content().json());
 
-    }
-	
+	}
+
 	@Test
-    public void testLoyaltyCardPostStatusIsOk() throws Exception {
+	public void testLoyaltyCardPostStatusIsOk() throws Exception {
+
 		LoyaltyCard loyaltyCard = new LoyaltyCard();
 		loyaltyCard.setName("pasindu");
-		loyaltyCard.setNumber("12345678"); 
+		loyaltyCard.setNumber("12345678");
 		loyaltyCard.setPointBalance(10.0);
 		loyaltyCard.setIssuedDate(new Date());
 		loyaltyCard.setExpiryDate(new Date());
-		
-		// Convert LoyaltyCard object into String
-		ObjectMapper mapper= new ObjectMapper();
-		String str = mapper.writeValueAsString(loyaltyCard);
-		
-        mockMvc.perform(MockMvcRequestBuilders.post("/loyalty/card")
-        		.contentType(MediaType.APPLICATION_JSON)
-        		.content(str))
-                .andExpect(MockMvcResultMatchers.status().isOk());
-//                .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON_VALUE))
-//                .andReturn();
-//                .andExpect(MockMvcResultMatchers.content().json());
 
-    }
+		// Convert LoyaltyCard object into String
+		ObjectMapper mapper = new ObjectMapper();
+		String loyaltyJson = mapper.writeValueAsString(loyaltyCard);
+//		System.out.println(loyaltyJson);
+
+		mockMvc.perform(MockMvcRequestBuilders.post("/loyalty/card").contentType(MediaType.APPLICATION_JSON)
+				.content(loyaltyJson)).andExpect(MockMvcResultMatchers.status().isOk());
+	}
+
+	@Test
+	public void testLoyaltyCardPostResponse() throws Exception {
+		LoyaltyCard loyaltyCardPost = new LoyaltyCard();
+		loyaltyCardPost.setName("pasindu");
+		loyaltyCardPost.setNumber("12345678");
+		loyaltyCardPost.setPointBalance(10.0);
+		loyaltyCardPost.setIssuedDate(null);
+		loyaltyCardPost.setExpiryDate(null);
+
+		LoyaltyCard loyaltyCardResponse = new LoyaltyCard();
+		loyaltyCardResponse.setId(1);
+		loyaltyCardResponse.setName("pasindu");
+		loyaltyCardResponse.setNumber("12345678");
+		loyaltyCardResponse.setPointBalance(10.0);
+		loyaltyCardResponse.setIssuedDate(null);
+		loyaltyCardResponse.setExpiryDate(null);
+		loyaltyCardResponse.setCategory(null);
+
+		// Convert LoyaltyCard object into String
+		ObjectMapper mapper = new ObjectMapper();
+		String jsonPost = mapper.writeValueAsString(loyaltyCardPost);
+		String jsonResponse = mapper.writeValueAsString(loyaltyCardResponse);
+		mockMvc.perform(
+				MockMvcRequestBuilders.post("/loyalty/card").contentType(MediaType.APPLICATION_JSON).content(jsonPost))
+				.andExpect(MockMvcResultMatchers.content().json(jsonResponse));
+
+	}
 }
